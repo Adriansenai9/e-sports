f11eb9a670b5ca9433b0176d439d7c18c80711c8
Adriansenai9 <128429963+Adriansenai9@users.noreply.github.com>
2023-04-03 16:43:26 -0300

    Update inova.js

---
+++ inova.js
@@ -0,0 +1,13 @@
+const lista = document.getElementById("lista")
+const image = document.querySelectorAll(".item")
+
+let index = 0
+
+function carrossel(){
+    index++
+    if(index > 1){
+        index = 0
+    }
+    lista.style.transform = `translateX(${-index * 970}px)`
+}
+setInterval(carrossel, 5000)
